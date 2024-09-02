84829a13c7b2e3c8f687c02ee65f32ee6e8dc1e0
harsha-devstar <harshithadeveloper20@gmail.com>
2024-09-02 08:17:24 +0530

    commit

--- Rotating_NavBar/script.js
+++ Rotating_NavBar/script.js
@@ -18,3 +18,34 @@ searchBtn.addEventListener('click', ()=>{
     searchEl.classList.toggle('active');
     searchField.focus();
 })
+
+
+
+//blurred image loading
+
+const img = document.querySelector('.img');
+const loading = document.querySelector('.loading');
+
+let loadPercent = 0;
+
+let interval = setInterval(blurringEffect, 30);
+function blurringEffect(){
+    loadPercent++;
+    if(loadPercent > 99){
+        clearInterval(interval);
+    }
+    // console.log(loadPercent);
+    loading.innerHTML = `${loadPercent}%`;
+    loading.style.opacity = scale(loadPercent, 1, 100, 1, 0);
+
+    img.style.filter =  `blur(${scale(loadPercent, 0, 100, 30, 0)}px)`;
+
+}
+
+//mapping function
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+
+
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
